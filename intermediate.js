db5d4a7896097197be9f4e538fa1fdf5923a7c91
Shwetank Chandra <96305925+Uzakun@users.noreply.github.com>
2023-06-18 14:12:22 +0530

    Learn Intermediate JS

---
+++ intermediate.js
@@ -0,0 +1,77 @@
+
+                               // Random number generation
+//  var n = Math.random();
+//  n *= 6;
+// n = Math.floor(n);
+// alert(n + 1);
+
+
+
+// var names = ["Uza", "Shwetank", "Namita", "Divyanshu"];
+
+// var guestName = prompt("What's your name?")
+
+// if (names.includes(guestName)) {
+//     alert("Enter");
+// } else {
+//     alert("Fuck Off");
+// }
+
+
+//                                                FizzBuzz
+// var output = [];
+// var count = 1; 
+
+
+// function fizzBuzz(){
+ 
+//  while( count <= 100){ 
+//  if(count%3 === 0 && count%5 === 0){
+//   output.push("FIZZBUZZ");
+//  }
+//  else if(count%3 === 0){
+//   output.push("Fizz");
+//  }
+//  else if(count%5 === 0){
+//   output.push("Buzz");
+//  }
+
+//  else{
+//   output.push(count);
+//  }
+
+
+//  count++;
+//  }
+//   console.log(output);
+
+  
+// } 
+
+
+
+
+
+//                                          fibonacci Series
+// function fibonacciGenerator(n) {
+//   var output = [];
+//   if (n === 1) {
+//     output = [0];
+//   }
+//   else if (n === 2) {
+//     output = [0, 1];
+//   }
+
+//   else {
+//     output = [0, 1];
+
+//     for (var i = 2; i < n; i++) {
+//       output.push(output[output.length - 2] + output[output.length - 1]);
+//     }
+
+//   }
+//   return output;
+// }
+
+// output = fibonacciGenerator(7);
+// console.log(output);
